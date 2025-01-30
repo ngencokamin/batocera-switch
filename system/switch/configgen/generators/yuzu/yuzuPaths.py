64172f5b76ebd2d40bd187b77c32287d85f11608
Nix Genco-Kamin <ngencokamin@gmail.com>
2025-01-30 17:14:58 -0600

    Initial generator overhaul, paths update, add controller applet feature

--- system/switch/configgen/generators/yuzu/yuzuPaths.py
+++ system/switch/configgen/generators/yuzu/yuzuPaths.py
@@ -7,9 +7,9 @@ from configgen.batoceraPaths import BIOS, CONFIGS, ROMS, SAVES
 
 YUZU_CONFIG: Final = CONFIGS / "yuzu"
 YUZU_ROMDIR: Final = ROMS / "switch"
-YUZU_SAVES:  Final = SAVES / 'switch'
+YUZU_SAVES:  Final = SAVES / 'switch' / 'yuzu'
 YUZU_KEYS: Final = BIOS / 'switch'
 YUZU_FIRMWARE: Final = BIOS / 'switch' / 'firmware'
-YUZU_DATA_DIR: Final = Path('/userdata/system/switch/yuzu.AppImage')
-YUZU_EA_DATA_DIR: Final = Path('/userdata/system/switch/yuzuEA.AppImage')
-CEMU_CONTROLLER_PROFILES: Final = CONFIGS / 'yuzu' / 'controllerProfiles'
+YUZU_APPIMAGE: Final = Path('/userdata/system/switch/yuzu.AppImage')
+YUZU_EA_APPIMAGE: Final = Path('/userdata/system/switch/yuzuEA.AppImage')
+YUZU_CONTROLLER_PROFILES: Final = CONFIGS / 'yuzu' / 'controllerProfiles'
